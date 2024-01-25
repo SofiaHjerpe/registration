8d44d3ecbd95eb41aa3782261705a48cde739d20
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-25 15:08:01 +0100

    add object and more styling, disable btn on error

--- README.md
+++ README.md
@@ -1,26 +1,3 @@
 # registration
 
-# Account Registration
-
-
-
-
-6. The `confirm password` must be validated to be identical as the `password`. If that's not the case, the input should receive appropriate styling to visualize this for the user, much like the previous part.
-
-7. If the `password` or the `confirm password` has not been correctly typed, the submit button should be disabled.
-
-8. There must be a submit event in the application. When the form is submitted all of the data should be presented in an object like this:
-
-```js
-const registrationData = {
-  name: "first name last name",
-  username: "username",
-  email: "email@email.com",
-  password: "password",
-};
-```
-
-This object is to be written to the console or written in an alert. The values should of course be corresponding to the values that the user have typed.
-
-9. I would like that you also focus on writing clean code and use decent, thought-through, stylings.
     
--- form.js
+++ form.js
@@ -6,24 +6,35 @@ let passwordValue = document.querySelector(".password");
 let confirmPassword = document.querySelector(".confirm-password");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  let btn = document.querySelector(".btn");
   const user = userName.value;
   const name = nameValue.value;
   const email = emailValue.value;
   const passwordVal = passwordValue.value;
   const confirm = confirmPassword.value;
-  let error = document.querySelector(".form p");
+  let error = document.querySelector(".err");
   const errMessage = "Password must be atleast 8 characters.";
   const err = "Your passwords does not match";
   let valid = true;
   switch (valid) {
     case passwordVal.length < 8:
       error.innerText = errMessage;
+      btn.classList.add("disabled");
       break;
 
     case passwordVal !== confirm:
       error.innerText = err;
+      btn.classList.add("disabled");
       break;
     default:
       error.innerText = "";
+      btn.classList.remove("disabled");
   }
+  const registrationData = {
+    name: name,
+    username: user,
+    email: email,
+    password: passwordVal,
+  };
+  console.log(registrationData);
 });
